10a5743a0fac34a4d60010d27cb9ca135db280df
Jagdeep59 <jagdeep_singh73@yahoo.com>
2026-01-04 22:50:02 +0530

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+window.alert("this is alert method") // this is a alert method
+
+document.writeln('hello i \"am here\"') // this write the text 
+
+var A = "this is a string";
+
+document.writeln(A); // it display the A variable
+
+var S = "this is Another string to alert"
+
+window.alert(S); // this is another alert method which come with pop up
+
+document.writeln("She said, \"It's a beautiful day!\"");
+
+document.writeln("\"Be who you are and say what you feel," + "because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seuss")
+
+var B = "concatenated" + " string"
+
+document.writeln(B) // for check concetination
+
+var family = "singh" , wife = "seemaa" , me = "jagdeep";
+
+document.writeln(wife) // multiple variable in line 
+
+document.writeln(3+7) // expression
